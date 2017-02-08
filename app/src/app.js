5186aaf9f3e6502ff4512444d71a39ba22d70c64
kanchansingh1123 <kanchan.singh@walkingtree.in>
2017-02-08 16:44:38 +0530

    Initial Commit

---
+++ app/src/app.js
@@ -0,0 +1,111 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SmbDashboard
+ * @description
+ * # Initializes main application and routing
+ *
+ * Main module of the application.
+ */
+
+// Example to require lodash
+// This is resolved and bundled by browserify
+//
+// var _ = require( 'lodash' );
+
+angular.module( 'SmbDashboard', [
+  'ionic',
+  'ngCordova',
+  'ngResource'
+] )
+.run( [
+  '$ionicPlatform',
+
+  function( $ionicPlatform )
+  {
+
+  $ionicPlatform.ready(function() {
+    // save to use plugins here
+  });
+
+  // add possible global event handlers here
+
+} ] )
+
+.config( [
+  '$httpProvider',
+  '$stateProvider',
+  '$urlRouterProvider',
+
+  function( $httpProvider, $stateProvider, $urlRouterProvider )
+  {
+    // register $http interceptors, if any. e.g.
+    // $httpProvider.interceptors.push('interceptor-name');
+
+    // Application routing
+    $stateProvider
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/main.html',
+        controller: 'MainController'
+      })
+      .state('app.home', {
+        url: '/home',
+        cache: true,
+        views: {
+          'home-tab': {
+            templateUrl: 'templates/views/home.html',
+            controller: 'HomeController'
+          }
+        }
+      })
+      .state('app.settings', {
+        url: '/settings',
+        cache: true,
+        views: {
+          'settings-tab': {
+            templateUrl: 'templates/views/settings.html',
+            controller: 'SettingsController'
+          }
+        }
+      })
+      .state('app.orders', {
+        url: '/orders',
+        cache: true,
+        views: {
+          'home-tab': {
+            templateUrl: 'templates/views/orders.html',
+            controller: 'SettingsController'
+          }
+        }
+      })
+      .state('app.redemptions', {
+        url: '/redemptions',
+        cache: true,
+        views: {
+          'home-tab': {
+            templateUrl: 'templates/views/settings.html',
+            controller: 'SettingsController'
+          }
+        }
+      });
+
+
+    // redirects to default route for undefined routes
+    $urlRouterProvider.otherwise('/app/home');
+  }
+] )
+
+// Angular module controllers
+//
+.controller( 'MainController',     require( './controllers/mainController'     ) )
+.controller( 'HomeController',     require( './controllers/homeController'     ) )
+.controller( 'SettingsController', require( './controllers/settingsController' ) )
+
+// Angular module services
+//
+.factory( 'ExampleService',        require( './services/ExampleService' ) )
+.factory( 'ApiService',            require( './services/ApiService'     ) )
+;
